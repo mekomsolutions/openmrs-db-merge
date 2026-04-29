2a8f8691cefe54484164341175fd2bab3b23eb48
wluyima <wyclif@openmrs.org>
2026-04-28 20:56:52 -0500

    Deleted unused DB accessor classes and moved Launcher to its own package

--- src/main/java/net/mekomsolutions/db/importer/bootstrap/Launcher.java
+++ src/main/java/net/mekomsolutions/db/importer/bootstrap/Launcher.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.mekomsolutions.db.importer;
+package net.mekomsolutions.db.importer.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
