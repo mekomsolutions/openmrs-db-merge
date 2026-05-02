7570413f5378ed6b0c324219028a732f98c434d6
wluyima <wyclif@openmrs.org>
2026-05-01 20:00:14 -0500

    Added initial schema for test tables

--- src/test/java/net/mekomsolutions/db/importer/BaseMergeTest.java
+++ src/test/java/net/mekomsolutions/db/importer/BaseMergeTest.java
@@ -1,9 +1,16 @@
 package net.mekomsolutions.db.importer;
 
+import java.time.LocalDateTime;
+
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.job.SimpleJob;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
+import lombok.extern.slf4j.Slf4j;
 import net.mekomsolutions.db.importer.batch.BatchConfig;
 
 @Import(BatchConfig.class)
@@ -11,4 +18,24 @@ import net.mekomsolutions.db.importer.batch.BatchConfig;
 @TestPropertySource(properties = "batch.read.size=10")
 @TestPropertySource(properties = "batch.write.size=10")
 @ComponentScan(basePackages = { "net.mekomsolutions.db.importer.helpers" })
-public abstract class BaseMergeTest extends BaseDbBackedTest {}
+@Slf4j
+public abstract class BaseMergeTest extends BaseDbBackedTest {
+	
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+	private SimpleJob job;
+	
+	protected void executeJob() throws Exception {
+		if (job.getStepNames().isEmpty()) {
+			log.info("No tables found containing data to import.");
+			return;
+		}
+		
+		log.info("Starting the job to import {} tables", job.getStepNames());
+		JobParametersBuilder builder = new JobParametersBuilder().addLocalDateTime("timestamp", LocalDateTime.now());
+		jobLauncher.run(job, builder.toJobParameters());
+	}
+	
+}
