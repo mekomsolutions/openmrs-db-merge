e661122004f253d3081e962c103962ec1d625188
wluyima <wyclif@openmrs.org>
2026-05-07 10:04:21 -0500

    Added visit related test data and test scenarios

--- src/test/java/net/mekomsolutions/db/importer/MergeTest.java
+++ src/test/java/net/mekomsolutions/db/importer/MergeTest.java
@@ -18,8 +18,12 @@ public class MergeTest extends BaseMergeTest {
 	private static final String QUERY_USER = "select * from users where user_id > 2 and uuid != '" + Constants.PHANTOM_UUID
 	        + "'";
 	
-	private static final String QUERY_PATIENT = "select * from patient where patient_id in (select person_id "
-	        + "from person where person_id != 1 and uuid != '" + Constants.PHANTOM_UUID + "')";
+	private static final String PATIENT_SUB_QUERY = "select person_id from person where person_id != 1 and uuid != '"
+	        + Constants.PHANTOM_UUID + "'";
+	
+	private static final String QUERY_PATIENT = "select * from patient where patient_id in (" + PATIENT_SUB_QUERY + ")";
+	
+	private static final String QUERY_VISIT = "select * from visit where patient_id in (" + PATIENT_SUB_QUERY + ")";
 	
 	@Autowired
 	@Qualifier("sourceJdbcTemplate")
@@ -30,26 +34,31 @@ public class MergeTest extends BaseMergeTest {
 	private JdbcTemplate sinkJdbcTemplate;
 	
 	@Test
-	@Sql({ "classpath:users.sql", "classpath:patient.sql" })
+	@Sql({ "classpath:users.sql", "classpath:patient.sql", "classpath:visit.sql", "classpath:encounter.sql" })
 	public void shouldMergeAllRowsInAllTables() throws Exception {
 		//Tables -> person,users,user_role,user_property,patient,visit,encounter
 		List<Map<String, Object>> persons = sinkJdbcTemplate.queryForList("select * from person where person_id > 1");
 		List<Map<String, Object>> users = sinkJdbcTemplate.queryForList("select * from users where user_id > 2");
 		List<Map<String, Object>> patients = sinkJdbcTemplate.queryForList("select * from patient");
+		List<Map<String, Object>> visits = sinkJdbcTemplate.queryForList("select * from visit");
 		Assertions.assertTrue(persons.isEmpty());
 		Assertions.assertTrue(users.isEmpty());
 		Assertions.assertTrue(patients.isEmpty());
+		Assertions.assertTrue(visits.isEmpty());
 		
 		executeJob();
 		
 		persons = sinkJdbcTemplate.queryForList(QUERY_PERSON);
 		users = sinkJdbcTemplate.queryForList(QUERY_USER);
 		patients = sinkJdbcTemplate.queryForList(QUERY_PATIENT);
+		visits = sinkJdbcTemplate.queryForList(QUERY_VISIT);
 		//All source DB persons including the row associated to admin and daemon
 		Assertions.assertEquals(11, persons.size());
 		//All source DB users including admin, excludes daemon
 		Assertions.assertEquals(6, users.size());
 		Assertions.assertEquals(5, patients.size());
+		Assertions.assertEquals(5, visits.size());
+		//TODO Compare sink row values with those from the source
 	}
 	
 }
--- src/test/resources/metadata.sql
+++ src/test/resources/metadata.sql
@@ -7,3 +7,9 @@ VALUES (1,'Question',1,'2026-01-01 00:00:00', 0,'1df33f36-496b-11f1-b548-0242ac1
 INSERT INTO concept (concept_id,datatype_id,class_id,is_set,creator,date_created,retired,uuid)
 VALUES (1,1,1,0,1,'2026-01-01 00:00:00', 0,'14976266-496c-11f1-b548-0242ac140002');
 
+INSERT INTO visit_type (visit_type_id,name,creator,date_created,retired,uuid)
+VALUES (1,'Adult Initial', 1,'2026-01-01 00:00:00',0,'167015d7-496b-11f1-b548-0242ac140002');
+
+INSERT INTO location (location_id,name,creator,date_created,retired,uuid)
+VALUES (1,'Test Clinic', 1,'2026-01-01 00:00:00',0,'167015d7-496b-11f1-b548-0242ac140002');
+
