3c892df73f73114c45a978ea7209ff3a2abb8d54
wluyima <wyclif@openmrs.org>
2026-05-19 14:15:12 -0500

    Implemented a method to retrieve id and uuid for all rows in a specific table

--- src/main/java/net/mekomsolutions/db/importer/helpers/BaseDbHelper.java
+++ src/main/java/net/mekomsolutions/db/importer/helpers/BaseDbHelper.java
@@ -1,5 +1,8 @@
 package net.mekomsolutions.db.importer.helpers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +36,29 @@ public abstract class BaseDbHelper {
 		}
 		catch (Exception e) {
 			String msg = "Failed to check if " + name + " table " + table + " is empty";
+			throw new RuntimeException(msg, e);
+		}
+	}
+	
+	/**
+	 * Retrieves all rows from the specified database table, returning only the primary key values and
+	 * uuids.
+	 *
+	 * @param tableName the name of the database table to query
+	 * @param pkColName the primary key column name
+	 * @return a list of rows, where each row is represented as a Map<String, Object>
+	 */
+	public List<Map<String, Object>> getAllRows(String tableName, String pkColName) {
+		if (log.isDebugEnabled()) {
+			log.debug("Retrieving {} and uuid for all rows from {} table {}", pkColName, name, tableName);
+		}
+		
+		String query = String.format("SELECT %s,uuid FROM %s", pkColName, tableName);
+		try {
+			return jdbcTemplate.queryForList(query);
+		}
+		catch (Exception e) {
+			String msg = "Failed to retrieve " + pkColName + " and uuids for rows from " + name + " table " + tableName;
 			throw new RuntimeException(msg, e);
 		}
 	}
